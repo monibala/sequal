a56e240dc67a7fe9c9ecfc05ca03a0cc896b55a8
Monika <Monika.cyberflax@gmail.com>
2022-08-16 16:54:14 +0530

    changes

--- sequal/settings.py
+++ sequal/settings.py
@@ -158,6 +158,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
