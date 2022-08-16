3c3c4b873bca59483b78a6db511a5321ea1ebd84
Monika <Monika.cyberflax@gmail.com>
2022-08-16 14:21:25 +0530

    changes

--- sequal/settings.py
+++ sequal/settings.py
@@ -10,8 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
-import django_heroku
-
+import django_on_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -164,4 +163,4 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 PAYPAL_RECEIVER_EMAIL = 'sb-1pdsh8805287@business.example.com'
-django_heroku.settings(locals())
+django_on_heroku.settings(locals())
